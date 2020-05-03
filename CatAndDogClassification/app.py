af7b12b58b69eefa3bc2f36db785a751a5d3b3db
Duy Phuc <phuc.tran@yomi.studio>
2020-05-03 18:21:59 +0700

    Add drag image to upload

--- CatAndDogClassification/app.py
+++ CatAndDogClassification/app.py
@@ -1,10 +1,26 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, session, url_for
 from flask import send_from_directory
 import numpy as np
 import tensorflow as tf
+from flask_dropzone import Dropzone
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 
 app = Flask(__name__)
+dropzone = Dropzone(app)
+app.config['SECRET_KEY'] = 'supersecretkeygoeshere'
+
+app.config['DROPZONE_UPLOAD_MULTIPLE'] = False
+app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
+app.config['DROPZONE_REDIRECT_VIEW'] = 'upload_file'
+
+# Uploads settings
+app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/uploads'
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)  # set maximum file size, default is 16MB
+
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 UPLOAD_FOLDER = 'uploads'
@@ -49,32 +65,59 @@ def classify(model, image_path):
     return label, float(classified_prob)
 
 # home page
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    # set session for image results
+    if "file_urls" not in session:
+        session['file_urls'] = []
+        session['files'] = []
+    # list to hold our uploaded image urls
+    file_urls = session['file_urls']
+    files = session['files']
+    # handle image upload from Dropszone
+    if request.method == 'POST':
+        file_obj = request.files
+        for f in file_obj:
+            file = request.files.get(f)
+
+            # save the file with to our photos folder
+            filename = photos.save(
+                file,
+                name=file.filename
+            )
+
+            # append image urls
+            file_urls.append(photos.url(filename))
+            files.append(filename)
+
+        session['file_urls'] = file_urls
+        session['files'] = files
+        print(file_urls)
+        return "uploading..."
+    # return dropzone template on GET request
+
     return render_template('home.html')
 
 
-@app.route('/classify', methods=['POST', 'GET'])
+@app.route('/classify')
 def upload_file():
+    if "file_urls" not in session or session['file_urls'] == []:
+        return redirect(url_for('home'))
 
-    if request.method == 'GET':
-        return render_template('home.html')
-    else:
-        file = request.files["image"]
-        upload_image_path = os.path.join(UPLOAD_FOLDER, file.filename)
-        file.save(upload_image_path)
-        print(upload_image_path)
-        label, prob = classify(cnn_model, upload_image_path)
-        print(label)
-        print(prob)
-        prob = round((prob * 100), 2)
+    # set the file_urls and remove the session variable
+    file_urls = session['file_urls']
+    files = session['files']
+    session.pop('file_urls', None)
+    session.pop('files', None)
 
-    return render_template('classify.html', image_file_name=file.filename, label=label, prob=prob)
+    file = files[0]
+    print(file)
+    file_url = file_urls[0]
+    upload_image_path = os.path.join(UPLOAD_FOLDER, file)
+    label, prob = classify(cnn_model, upload_image_path)
+    prob = round((prob * 100), 2)
 
-
-@app.route('/classify/<filename>')
-def send_file(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename)
+    return render_template('classify.html', image_file_name=file_url, label=label, prob=prob)
 
 
 if __name__ == '__main__':
